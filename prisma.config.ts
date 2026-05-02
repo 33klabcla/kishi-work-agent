4cf5b0f00db6b4dedbc011e215f414979154521c
33klabcla <qi4labo@gmail.com>
2026-05-02 12:28:45 +0900

    init050204

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    // Prisma v7 ではここに DB URL を渡す必要があり、環境変数が必須なので `!` で型を絞る
-    url: process.env.DATABASE_URL!,
+    url: process.env.DIRECT_URL!,
   },
 });
